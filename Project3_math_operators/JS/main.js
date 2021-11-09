e539ee2b941f9e55b0fceabfbd9b484de4837222
KarimBollas <67483536+KarimBollas@users.noreply.github.com>
2021-11-09 10:36:33 -0800

    PROJECT3_MATH_OPERATORS SUBMISSION ASSIGNMENT

--- Project3_math_operators/JS/main.js
+++ Project3_math_operators/JS/main.js
@@ -1,42 +1,62 @@
+// Global Variables o increment and decrement functions
 var i=0;
 var d=9999;
-
-function Add()                                        
+// Secction of Functions
+function Add()                                                        // Define Addition function
 {           
     var result=2+3;    
     document.getElementById("Add").innerHTML=" 2+3 = "+result;                                                        
 }
-function Sub()                                        
+function Sub()                                                        // Define Subtraction function
 {           
     var result=5-2;    
     document.getElementById("Sub").innerHTML=" 5-2 = "+result;                                                        
 }
-function Mul()                                        
+function Mul()                                                        // Define Multiplication function                                        
 {           
     var result=6*8;    
     document.getElementById("Mul").innerHTML=" 6*8 = "+result;                                                        
 }
-function Div()                                        
+function Div()                                                        // Define Divition function                                        
 {           
     var result=48/6;    
     document.getElementById("Div").innerHTML=" 48/6 = "+result;                                                        
 }
-function All()                                        
+function All()v                                                          // Define Addition Subtraction Multiplication and Divition function                                     
 {           
     var result=(1+2)*10/2-5;    
     document.getElementById("All").innerHTML=" (1+2)*10/2-5 = "+result;                                                        
 }
-function Mod()                                        
+function Mod()                                                        // Define Modulus function
 {           
     var result=25%6;    
     document.getElementById("Mod").innerHTML=" When you divide 15 by 6 you have a remainder of: "+result;                                                        
 }
-function Neg()                                        
+function Neg()                                                        // Define Negation function                                        
 {           
     var result=10;    
     document.getElementById("Neg").innerHTML=-result;                                                        
 }
-function Text_Reset()                                        
+function Inc()                                                        // Define Increment function with global variables to increment each mouseenter event                                         
+{           
+    i++;    
+    document.getElementById("Inc").innerHTML=i + " Increments  (Hover me)";                                                        
+}
+function Dec()                                                      // Define Decrement function with global variables to decrement each mouseenter event                                        
+{           
+    d--;    
+    document.getElementById("Dec").innerHTML=`${d} Decremets Counter (Hover me, start at 9999)`;                                                        
+}
+function Ran()                                                      // Define Random function                                                                                 
+{                  
+    document.getElementById("Ran").innerHTML= Math.trunc((Math.random()*100));    // Math object with  trunc and random methods                                                     
+}
+function Eul()                                                      // Define Euler function
+{           
+       
+    document.getElementById("Eul").innerHTML=Math.E;                // Euler Constant from Math Object
+}
+function Text_Reset()                                               // Reset the labels 
 {               
     document.getElementById("Add").innerHTML="Addition (Hover me)";
     document.getElementById("Sub").innerHTML="Subtraction (Hover me)";
@@ -45,5 +65,8 @@ function Text_Reset()
     document.getElementById("All").innerHTML="Several (Hover me)";
     document.getElementById("Mod").innerHTML="Modulus (Hover me)";
     document.getElementById("Neg").innerHTML="Negation (Hover me)";
-    document.getElementById("Neg").innerHTML="Negation (Hover me)";
+    document.getElementById("Inc").innerHTML=i + " Increments (Hover me)";
+    document.getElementById("Dec").innerHTML=d+ " Decremets Counter (Hover me, start at 9999)";
+    document.getElementById("Ran").innerHTML="Random (Hover me)";
+    document.getElementById("Eul").innerHTML="Euler number by Math.E (Hover me)";
 }
